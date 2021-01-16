562fbf42129373ae604b6aaac5f70febfceb5d3b
nickpsal <nickpsal@gmail.com>
2021-01-16 15:02:21 +0200

    commit 4

--- index.php
+++ index.php
@@ -38,6 +38,13 @@
                 </div>
             </form>
         </div>
+        <div class="container">
+            <footer class="bg-light text-center text-lg-start">
+                <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+                    © <?php echo date("Y"); ?> Created by nickpsal
+                </div>
+            </footer>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     </body>
