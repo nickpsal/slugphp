62103d51095937e54d8c9084f3f941ebb5589b2e
nickpsal <nickpsal@gmail.com>
2021-01-16 14:49:26 +0200

    commit 3

--- index.php
+++ index.php
@@ -15,30 +15,30 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <title>Slug Generator</title>
-</head>
-<body>
-    <div class="container">
-        <H1 class="page-header">Slug Generator</H1>
-        <form action="" method="post">
-            <div class='form-group'>
-                <label for="title" class="for">Title</label>
-                <input type="text" class="form-control" name="title" required value="<?php echo $title; ?>">
-            </div>
-            <div class='form-group'>
-                <input type="text" class="form-control" name="title_slug" disabled value="<?php echo $slug; ?>">
-            </div>
-            <div class='form-group'>
-                <input class = 'btn btn-primary' type = 'submit' name = 'generate_slug' value = 'Generate Slug'>
-                <input class = 'btn btn-primary' type = 'submit' name = 'clear' value = 'Clear'>
-            </div>
-        </form>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <title>Slug Generator</title>
+    </head>
+    <body>
+        <div class="container">
+            <H1 class="page-header">Slug Generator</H1>
+            <form action="" method="post">
+                <div class='form-group'>
+                    <label for="title" class="for">Title</label>
+                    <input type="text" class="form-control" name="title" required value="<?php echo $title; ?>">
+                </div>
+                <div class='form-group'>
+                    <input type="text" class="form-control" name="title_slug" disabled value="<?php echo $slug; ?>">
+                </div>
+                <div class='form-group'>
+                    <input class = 'btn btn-primary' type = 'submit' name = 'generate_slug' value = 'Generate Slug'>
+                    <input class = 'btn btn-primary' type = 'submit' name = 'clear' value = 'Clear'>
+                </div>
+            </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    </body>
 </html>
